12234ed29e09ad67bbd49926bf8090ad0d1c22e3
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-12-24 04:28:55 -0700

    Added the transformStylesForVNode function

--- src/ts/h.ts
+++ src/ts/h.ts
@@ -14,9 +14,9 @@ import {
  * @returns {IVNodeProps['simpleAttrs']}
  */
 function transformSimpleAttrsForVNode(
-  simpleAttributes: IHProps['simpleAttrs'],
+  simpleAttributes: IHProps['simpleAttrs'] = {},
 ): IVNodeProps['simpleAttrs'] {
-  return new Map<string, string>(Object.entries(simpleAttributes || {}));
+  return new Map<string, string>(Object.entries(simpleAttributes));
 }
 
 /**
@@ -25,9 +25,9 @@ function transformSimpleAttrsForVNode(
  * @returns {IVNodeProps['classes']}
  */
 function transformClassesForVNode(
-  classes: IHProps['classes'],
+  classes: IHProps['classes'] = [],
 ): IVNodeProps['classes'] {
-  return new Set<string>(classes || []);
+  return new Set<string>(classes);
 }
 
 /**
@@ -46,4 +46,24 @@ function transformEventsForVNode(
   const keys = Object.keys(events) as Array<keyof GlobalEventHandlersEventMap>;
   keys.forEach((key) => transformEvents.set(key, events[key] as EventListener));
   return transformEvents;
+}
+
+/**
+ * This function transform styles for a virtual dom.
+ * @param {IHProps['styles']} styles - Styles to be added to the html element.
+ * @returns {IVNodeProps['styles']}
+ */
+function transformStylesForVNode(
+  styles: IHProps['styles'] = {},
+): IVNodeProps['styles'] {
+  const transformStyles = new Map<keyof CSSStyleDeclaration, string>();
+  const keys = Object.keys(styles) as Array<keyof CSSStyleDeclaration>;
+
+  keys.forEach((key) => {
+    if (typeof styles[key] === 'string') {
+      transformStyles.set(key, styles[key] as string);
+    }
+  });
+
+  return transformStyles;
 }
