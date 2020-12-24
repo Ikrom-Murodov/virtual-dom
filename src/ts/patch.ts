9daad4e83d975941540b9093f3dfb64c50116dae
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-12-24 05:21:06 -0700

    Added the optimizationForSimpleAttributes function that is responsible for optimizing simple attributes.

--- src/ts/patch.ts
+++ src/ts/patch.ts
@@ -43,3 +43,48 @@ function optimizationForEvents(currentVNode: IVNode, newVNode: IVNode): void {
     }
   });
 }
+
+/**
+ * This function removes simple attributes that are not in the new virtual node.
+ *   And if the new virtual node has simple attributes that is in the current one,
+ *   the function checks their values for equality and if their values
+ *   are not equal, the function will overwrite the current values with a new one.
+ *
+ * @param { IVNode } currentVNode - A virtual node that was added to the real DOM using
+ *   the mount function.
+ *
+ * @param { IVNode } newVNode - New virtual node to change the previous one.
+ *
+ * @throws Throws an error if the current virtual node has not been added to
+ *   the current DOM using the mount function
+ *
+ * @returns { void } This function returns nothing.
+ */
+function optimizationForSimpleAttributes(
+  currentVNode: IVNode,
+  newVNode: IVNode,
+): void {
+  if (currentVNode.$el === undefined) {
+    throw new Error(
+      'First insert the virtual node into the dom using the mount function and then use this function',
+    );
+  }
+
+  newVNode.$el = currentVNode.$el;
+
+  currentVNode.props.simpleAttrs.forEach((currentAttrValue, attrName) => {
+    const newAttributeValue = newVNode.props.simpleAttrs.get(attrName);
+
+    if (newAttributeValue) {
+      if (newAttributeValue !== currentAttrValue) {
+        newVNode.$el?.setAttribute(attrName, newAttributeValue);
+      }
+    } else newVNode.$el?.removeAttribute(attrName);
+  });
+
+  newVNode.props.simpleAttrs.forEach((attrValue, attrName) => {
+    if (!currentVNode.props.simpleAttrs.has(attrName)) {
+      newVNode.$el?.setAttribute(attrName, attrValue);
+    }
+  });
+}
