89dfec2c6d404bc66edc4e6d8aab1d6c8064ea71
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-12-24 05:02:17 -0700

    Added the mount function that takes a virtual node and turns it into a real dom  node and then inserts it into the html element..

--- src/ts/mount.ts
+++ src/ts/mount.ts
@@ -1,4 +1,20 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import { IVNode } from './interfaces';
+
+/**
+ * This function takes a virtual node and turns it into a real dom
+ *   node and then inserts it into the html element.
+ *
+ * @param {IVNode} vNode - you can create virtual node with h function.
+ * @param { HTMLElement } container - Parent for the new html element.
+ * @returns {void} This function returns nothing.
+ */
+export function mount(vNode: IVNode, container: HTMLElement): void {
+  const htmlElement = createHtmlElementFromVNode(vNode);
+  container.append(htmlElement);
+  // eslint-disable-next-line
+  vNode.$el = htmlElement;
+}
 
 /**
  *  This function creates an html element from a virtual node.
